b0eeb20f1180933383afbb5cc750a7787b8faf75
keeeper <keeper96@gmail.com>
2023-07-23 18:06:53 +0200

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<main class="min-h-screen flex flex-col items-center justify-center">
+  <h1 class="text-4xl font-bold mb-4">Error {$page.status}: {$page.error?.message}</h1>
+</main>
