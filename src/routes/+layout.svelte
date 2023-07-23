661460d014c18d10c848d639429c0e2fc9d39a65
keeeper <keeper96@gmail.com>
2023-07-24 00:01:52 +0200

    menu fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,13 +8,13 @@
 
 <div class="min-h-screen flex flex-col">
   <div class="flex flex-col fixed top-5 right-0">
-    {#if $page.url.pathname !== "/"}
+    {#if $page?.url.pathname !== "/"}
       <a href="/" class="btn btn-neutral rounded-r-none mb-2">
         <img src={`/icons/home.svg`} alt="Home" width={24} height={24}} />
       </a>
     {/if}
-    {#if $currentUser?.uid && $page.url.pathname !== "/"}
-      <a href={`/${$userData.username}/edit`} class="btn btn-neutral rounded-r-none">
+    {#if $currentUser?.uid && $page?.url.pathname !== "/"}
+      <a href={`/${$userData?.username}/edit`} class="btn btn-neutral rounded-r-none">
         <img src={`/icons/edit.svg`} alt="Edit" width={24} height={24}} />
       </a>
     {/if}
